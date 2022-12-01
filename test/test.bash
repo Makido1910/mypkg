3f22662f4eb619ffd1e5711c4e113d9162c83ab2
Yusuke Makido <s21c1107za@s.chibakoudai.jp>
2022-12-01 12:00:47 +0900

    fix test.bash

--- test/test.bash
+++ test/test.bash
@@ -5,8 +5,8 @@ dir=~
 
 cd $dir/ros2_ws
 colcon build
-source $dir/./bashrc
+source $dir/.bashrc
 timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
-cat /temp/mypkg.log |
+cat /tmp/mypkg.log |
 grep 'Listen: 10'
