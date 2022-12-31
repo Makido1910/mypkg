ee78c829fd22af7e4421eac8ff443ed544aecb1c
Yusuke Makido <s21c1107za@s.chibakoudai.jp>
2022-12-31 17:48:31 +0900

    add a license

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -1,16 +1,22 @@
+#SPDX-FileCopyrightText: 2022 Makido1910
+#SPDX-License-Identifier: BSD-3-Clause
+
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int16
 
 
+
+    
 def cb(msg):
     global node
     node.get_logger().info("Listen: %d" % msg.data)
+        
 
 rclpy.init()
 node = Node("listener")
 sub = node.create_subscription(Int16, "countup", cb, 10)
 rclpy.spin(node)
 
-if _name_ == '_main_':
-    main()
+if __name__ == '__main__':
+   main()
